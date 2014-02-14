ddad21123a14a1dc96b6b2902efe498b051140d7
bodeamariuscosmin <bodeamariuscosmin@gmail.com>
2014-02-14 13:29:36 +0200

    desired_capabilities['version'] =  changed

--- Saucelabs Script Files/Windows 7/Firefox/Sign Up (saucelabs).py
+++ Saucelabs Script Files/Windows 7/Firefox/Sign Up (saucelabs).py
@@ -14,7 +14,7 @@ class Selenium2OnSauce(unittest.TestCase):
 
     def setUp(self):
         desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
-        desired_capabilities['version'] = '28'
+        desired_capabilities['version'] = '26'
         desired_capabilities['platform'] = 'Windows 7'
         desired_capabilities['name'] = 'Sign Up'
 
