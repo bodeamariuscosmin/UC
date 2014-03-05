cffbd9d73f3da21076993fb051f6ca524734bd6a
bodeamariuscosmin <bodeamariuscosmin@gmail.com>
2014-03-05 14:08:55 +0200

    Slash removed
    It seems that driver.get(self.base_url + "/") was adding a second / to
    the self.base_url and this failed the tests.

--- Saucelabs Script Files/OSX 10.9/Chrome/Sign Up (saucelabs).py
+++ Saucelabs Script Files/OSX 10.9/Chrome/Sign Up (saucelabs).py
@@ -24,7 +24,7 @@ class Selenium2OnSauce(unittest.TestCase):
             command_executor="http://mariusb:bd27d6b0-f987-4773-b20b-633da38327de@ondemand.saucelabs.com:80/wd/hub"
         )
         self.driver.implicitly_wait(30)
-        self.base_url = "https://staging.urbancompass.com/"
+        self.base_url = "https://staging.urbancompass.com"
 
     def test_sign_up(self):
         driver = self.driver
