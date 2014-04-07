f6009336dc20904b989cbc0373da071b4db1d92b
bodeamariuscosmin <bodeamariuscosmin@gmail.com>
2014-04-07 16:29:12 +0300

    Added Favorite Button (Sales) & (Rentals)
    Added Favorite Button (Sales) & (Rentals) test script files.

---
+++ Saucelabs Script Files/OSX 10.9/Firefox/Favorite button (Rentals).py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+from random import random
+from random import randint
+import random as rn
+my_string = 'string'
+
+class Selenium2OnSauce(unittest.TestCase):
+
+    def setUp(self):
+        desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
+        desired_capabilities['version'] = '26'
+        desired_capabilities['platform'] = 'OS X 10.9'
+        desired_capabilities['name'] = 'Favorite Button (Rentals)'
+
+        self.driver = webdriver.Remote(
+            desired_capabilities=desired_capabilities,
+            command_executor="http://mariusb:bd27d6b0-f987-4773-b20b-633da38327de@ondemand.saucelabs.com:80/wd/hub"
+        )
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://staging.urbancompass.com/"
+    
+    def test_favorite_contact_to_visit(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Sign up").click()
+        driver.find_element_by_name("first").clear()
+        driver.find_element_by_name("first").send_keys("qa")
+        driver.find_element_by_name("last").clear()
+        driver.find_element_by_name("last").send_keys("test")
+        driver.find_element_by_xpath("(//input[@name='email'])[2]").clear()
+        driver.find_element_by_xpath("(//input[@name='email'])[2]").send_keys("qa+"+str(rn.randint(0,99999))+"@urbancompass.com")
+        driver.find_element_by_xpath("(//input[@name='password'])[2]").clear()
+        driver.find_element_by_xpath("(//input[@name='password'])[2]").send_keys("parola")
+        driver.find_element_by_css_selector("#sign-up > div.modal_content > form > input[type=\"submit\"]").click()
+        time.sleep(5)
+        driver.find_element_by_link_text("Rentals").click()
+        driver.find_element_by_link_text("List").click()
+        driver.find_element_by_id("address_search").click()
+        driver.find_element_by_id("address_search").send_keys("Chinatown")
+        driver.find_element_by_id("search_button").click()
+        driver.find_element_by_id("listing-position-A").click()
+        driver.find_element_by_css_selector(".ss-icon.ss-star").click()
+        driver.find_element_by_link_text("My Apartments").click()
+        driver.find_element_by_link_text("Favorites").click()
+        driver.find_element_by_xpath("//div[@id='container']/div/div[2]/div/div/div[2]/div/div[3]/div/div/a/div[5]").click()
+        driver.find_element_by_link_text("Request Status").click()
+        driver.find_element_by_css_selector("div.dropdown__link").click()
+        driver.find_element_by_link_text("Logout").click()
+    
+    def tearDown(self):
+        print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
