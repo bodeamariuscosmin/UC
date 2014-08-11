88494075547eb0675c25bf12418244a48b39f678
bodeamariuscosmin <bodeamariuscosmin@gmail.com>
2014-08-12 00:40:53 +0300

    Log In (Sales) fixed

--- Saucelabs Script Files/OSX 10.9/Chrome/Log In (saucelabs).py
+++ Saucelabs Script Files/OSX 10.9/Chrome/Log In (saucelabs).py
@@ -67,27 +67,7 @@ class Selenium2OnSauce(unittest.TestCase):
         driver.find_element_by_xpath(".//*[@id='manage_alerts']").click()
         driver.find_element_by_id("hamburger-navigation").click()
         driver.find_element_by_link_text("Logout").click()
-        '''driver.find_element_by_id("hamburger-navigation").click()
-        # Log in from the Contact Agent button
-        driver.find_element_by_link_text("Rentals").click()
-        driver.find_element_by_link_text("List").click()
-        driver.find_element_by_id("s2id_autogen1").click()
-        time.sleep(1)
-        driver.find_element_by_css_selector("#select2-result-label-3").click()
-        driver.find_element_by_id("listing-position-A").click()
-        driver.find_element_by_link_text("CONTACT AGENT").click()
-        time.sleep(5)
-        driver.find_element_by_xpath(".//*[@id='contact-to-visit-anonymous']/footer/p/a").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("qa+renter@urbancompass.com")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("parola")
-        # Agent allocation
-        driver.find_element_by_css_selector("#sign-in > div.modal_content > form > input[type=\"submit\"]").click()
-        driver.find_element_by_link_text("MY APARTMENTS").click()
-        time.sleep(2)
-        driver.find_element_by_link_text("Me").click()
-        driver.find_element_by_link_text("Logout").click()'''
+        
 
     def tearDown(self):
         print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
--- Saucelabs Script Files/Windows 7/Internet Explorer 9/Log In (Sales) (saucelabs).py
+++ Saucelabs Script Files/Windows 7/Internet Explorer 9/Log In (Sales) (saucelabs).py
@@ -1,4 +1,3 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,22 +5,21 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
 
-
 class Selenium2OnSauce(unittest.TestCase):
 
     def setUp(self):
-        desired_capabilities = webdriver.DesiredCapabilities.SAFARI
-        desired_capabilities['version'] = '7'
-        desired_capabilities['platform'] = 'OS X 10.9'
+        desired_capabilities = webdriver.DesiredCapabilities.INTERNETEXPLORER
+        desired_capabilities['version'] = '9'
+        desired_capabilities['platform'] = 'Windows 7'
         desired_capabilities['name'] = 'Log In (Sales)'
 
         self.driver = webdriver.Remote(
-            desired_capabilities = desired_capabilities,
-            command_executor = "http://mariusb:bd27d6b0-f987-4773-b20b-633da38327de@ondemand.saucelabs.com:80/wd/hub"
+            desired_capabilities=desired_capabilities,
+            command_executor="http://mariusb:bd27d6b0-f987-4773-b20b-633da38327de@ondemand.saucelabs.com:80/wd/hub"
         )
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(50)
         self.base_url = "https://staging.urbancompass.com/"
-        
+    
     def test_log_in_sales(self):
         driver = self.driver
         driver.get(self.base_url + "/")
@@ -30,16 +28,14 @@ class Selenium2OnSauce(unittest.TestCase):
         driver.find_element_by_link_text("Sales").click()
         driver.find_element_by_id("hamburger-navigation").click()
         driver.find_element_by_link_text("Log in").click()
-        driver.find_element_by_xpath(".//*[@id='sign-in']/div[2]/form/fieldset[1]/input").clear()
-        driver.find_element_by_xpath(".//*[@id='sign-in']/div[2]/form/fieldset[1]/input").send_keys("qa+renter@urbancompass.com")
-        driver.find_element_by_xpath(".//*[@id='sign-in']/div[2]/form/fieldset[2]/input").clear()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("qa+renter@urbancompass.com")
         driver.find_element_by_xpath(".//*[@id='sign-in']/div[2]/form/fieldset[2]/input").send_keys("parola")
-        driver.find_element_by_xpath(".//*[@id='sign-in']/div[2]/form/input").click()
-        time.sleep(2)
+        driver.find_element_by_css_selector("#sign-in > div.modal_content > form > input[type=\"submit\"]").click()        
         driver.find_element_by_id("hamburger-navigation").click()
-        time.sleep(1)
+        time.sleep(2)
         driver.find_element_by_link_text("Logout").click()
-
+    
     def tearDown(self):
         print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
         self.driver.quit()
